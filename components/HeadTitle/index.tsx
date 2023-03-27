957fabc514f68a4c4857744325d716116ac90226
Austin <rich.swift426@gmail.com>
2023-03-27 09:34:25 -0400

    create HeadTitle component

---
+++ components/HeadTitle/index.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from "react-native";
+import { useCallback, useEffect } from "react";
+
+import { useFonts } from "expo-font";
+
+type Props = {
+  children?: any;
+};
+const HeadTitle = (props: Props) => {
+  const { children } = props;
+  const [fontsLoaded, error] = useFonts({
+    LibreBodonie: require("../../assets/fonts/LibreBodoni-VariableFont_wght.ttf"),
+  });
+  useEffect(() => {
+    if (error) console.log(error);
+  }, [error]);
+  return (
+    <View>
+      <Text style={[styles.title, { fontFamily: "LibreBodonie" }]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    marginHorizontal: 16,
+    marginTop: 5,
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+});
+
+export default HeadTitle;
