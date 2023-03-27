33d5ed439fe2b38e0bcec7eb4a1e1816c58e2248
Austin <rich.swift426@gmail.com>
2023-03-27 09:35:18 -0400

    create ImageButton component

---
+++ components/ImageButton/index.tsx
@@ -0,0 +1,31 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const ImageButton = () => {
+  return (
+    <View style={styles.card}>
+      <TouchableOpacity>
+        <Image
+          style={styles.thumbs}
+          source={{ uri: "assets/images/image4.png" }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: "95%",
+    // height: 250,
+    marginLeft: 20,
+    marginBottom: 30,
+    borderRadius: 5,
+  },
+  thumbs: {
+    width: "100%",
+    height: 320,
+    borderRadius: 10,
+  },
+});
+
+export default ImageButton;
