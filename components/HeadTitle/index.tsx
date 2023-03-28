5512480470b8073ea97ec8edbf0afb9987594410
Austin <rich.swift426@gmail.com>
2023-03-28 08:38:00 -0400

    fixup font family

--- components/HeadTitle/index.tsx
+++ components/HeadTitle/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 const HeadTitle = (props: Props) => {
   const { children } = props;
   const [fontsLoaded, error] = useFonts({
-    LibreBodonie: require("../../assets/fonts/BOD_CB.ttf"),
+    LibreBodonie: require("../../assets/fonts/BOD_CB.TTF"),
   });
   useEffect(() => {
     if (error) console.log(error);
